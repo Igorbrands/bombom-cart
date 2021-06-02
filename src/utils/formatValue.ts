07eaa4e08dce4aa257dbfc22f4d16de0178b1ffd
igorbrands <igorcantelmo@gmail.com>
2021-06-02 20:07:45 -0300

    feat: utils to format values in reais

---
+++ src/utils/formatValue.ts
@@ -0,0 +1,7 @@
+const formatValue = (value: number): string =>
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value / 100);
+
+export default formatValue;
