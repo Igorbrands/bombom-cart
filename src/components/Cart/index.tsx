11584b783a6fe7f9a940059092a0594dabbe3e91
igorbrands <igorcantelmo@gmail.com>
2021-06-03 19:43:12 -0300

    feat: creates cart component

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,49 +1,67 @@
 import { CartProductType } from '../../pages/Dashboard';
 import CartItem from '../CartItem';
 import formatValue from '../../utils/formatValue';
+import { MdClear } from 'react-icons/md';
 
 import {
   Container,
+  CartHeader,
+  CartContent,
   TotalContainer,
   ContainerButton,
   FreeDeliver,
+  ContainerBottom,
 } from './styles';
 
 type CartProps = {
   cartItems: CartProductType[];
   addToCart: (clcikedProduct: CartProductType) => void;
   removeFromCart: (id: number) => void;
+  closeCart: () => void;
 };
 
-function Cart({ cartItems, addToCart, removeFromCart }: CartProps) {
+function Cart({ cartItems, addToCart, removeFromCart, closeCart }: CartProps) {
   const calcTotal = (products: CartProductType[]) =>
     products.reduce(
       (acc: number, product) => acc + product.amount * product.price,
       0
     );
+  const totalPrice = calcTotal(cartItems);
+  console.log(calcTotal(cartItems));
 
   return (
     <Container>
-      <h2>Meu carrinho</h2>
-      {cartItems.length === 0 ? <p>Carrinho vazio 🙁</p> : null}
-      {cartItems.map((product) => (
-        <CartItem
-          key={product.id}
-          item={product}
-          addToCart={addToCart}
-          removeFromCart={removeFromCart}
-        />
-      ))}
-      <TotalContainer>
-        <div>
-          <span>Total</span>
-          <span>{formatValue(calcTotal(cartItems))}</span>
-        </div>
-        <FreeDeliver>Parabéns, sua compra tem frete grátis !</FreeDeliver>
-      </TotalContainer>
-      <ContainerButton>
-        <button>Finalizar compra</button>
-      </ContainerButton>
+      <CartHeader>
+        <h2>Meu carrinho</h2>
+        <MdClear size={24} onClick={closeCart} />
+      </CartHeader>
+      <CartContent>
+        {cartItems.length === 0 ? <span>Carrinho vazio 🙁</span> : null}
+        {cartItems.map((product) => (
+          <CartItem
+            key={product.id}
+            item={product}
+            addToCart={addToCart}
+            removeFromCart={removeFromCart}
+          />
+        ))}
+      </CartContent>
+      <ContainerBottom>
+        <TotalContainer>
+          <div>
+            <span>Total</span>
+            <span>{formatValue(totalPrice)}</span>
+          </div>
+          {totalPrice > 1000 && (
+            <FreeDeliver>
+              <p>Parabéns, sua compra tem frete grátis !</p>
+            </FreeDeliver>
+          )}
+        </TotalContainer>
+        <ContainerButton>
+          <button>Finalizar compra</button>
+        </ContainerButton>
+      </ContainerBottom>
     </Container>
   );
 }
--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -1,30 +1,83 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const Container = styled.aside`
-  width: 500px;
-  padding: 20px;
+  width: 25rem;
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+
+  @media (max-width: 800px) {
+    width: 100vw;
+  }
+`;
+export const CartHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border-bottom: 1px solid #c2c2c2;
+  padding: 1rem;
+  flex: 0 1 auto;
+  svg {
+    cursor: pointer;
+    position: fixed;
+    top: 0.625rem;
+    right: 0.625rem;
+  }
+`;
+
+export const CartContent = styled.div`
+  flex: 1 1 auto;
+`;
+export const ContainerBottom = styled.div`
+  flex: 0 1 auto;
 `;
 
 export const TotalContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   flex-direction: column;
   border-top: 1px solid #c2c2c2;
+  padding: 1.5rem;
 
   > div {
     display: flex;
     justify-content: space-between;
+    margin-bottom: 0.75rem;
+  }
+  div + div {
+    justify-content: center;
   }
 `;
 export const ContainerButton = styled.div`
   border-top: 1px solid #c2c2c2;
+  padding: 1.5rem;
+  button {
+    all: unset;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: #3b74f2;
+    color: #fff;
+
+    border-radius: 0.25rem;
+    padding: 0.5rem;
+    width: 100%;
+    cursor: pointer;
+    transition: all 0.2s;
+    :hover {
+      transform: scale(1.01);
+      opacity: 0.9;
+    }
+    :active {
+      transform: scale(0.99);
+    }
+  }
 `;
 
 export const FreeDeliver = styled.div`
-  display: flex;
-  align-self: center;
-  padding: 8px;
-  border-radius: 24px;
+  padding: 0.5rem;
+  border-radius: 1.5rem;
   background: #c7ffa6;
   color: #508800;
+  font-size: 0.875rem;
 `;
